703f2e8a5f2a97a462dbcab5bd2cac39a8ff54a4
atorr <alexorr.cse@gmail.com>
2018-03-17 20:45:30 -0700

    Added consensus algorithm and node registration as well as fixing syntax errors in other untested parts

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # StupendousChain
 Perhaps the best, most impressive and effective chain there ever were.
+
+Just a simple blockchain project to learn the ins and outs of the tech.  
+
+## What can it do now?
+Not a whole lot.  It's just starter code.  
+
+
--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,8 @@
 import hashlib
 import json
+import requests
+
+from urllib.parse import urlparse
 from textwrap import dedent
 from time import time
 from uuid import uuid4
@@ -10,10 +13,11 @@ class Blockchain(object):
   def __init__(self):
     self.chain = []
     self.current_transactions = []
+    self.nodes = set()
 
     # Genesis Block
     self.new_block(previous_hash=1, proof=100)
-  
+ 
   def new_block(self, proof, previous_hash=None): 
     """
       Create a new Block in the Blockchain
@@ -55,6 +59,78 @@ class Blockchain(object):
 
     return self.last_block['index'] + 1
    
+  def register_node(self, address):
+    """
+      Add a new node to the list of nodes
+      
+      :param address: <str> Address of node.  
+                      Eg. 'http://192.168.9.5:5000'
+      :return: None
+    """
+    parsed_url = urlparse(address)
+    self.nodes.add(parsed_url.netloc)
+
+  def valid_chain(self, chain):
+    """
+      Determine if a given blockchain is valid
+
+      :param chain: <list> A blockchain
+      :return: <bool> True if valid, False if not
+    """
+
+    prev_block = chain[0]
+    current_index = 1
+    
+    while current_index < len(chain):
+      block = chain[current_index]
+      print(f'{prev_block}')
+      print(f'{block}')
+      print("\n-----------\n")
+      # check that the hash of the block is correct
+      if block['previous_hash'] != self.hash(prev_block):
+        return False
+      
+      # Check that the Proof of Work is correct
+      if not self.valid_proof(prev_block['proof'], block['proof']):
+        return False
+
+      prev_block = block
+      current_index += 1
+      
+    return True
+
+  def resolve_conflicts(self):
+    """
+      This is our Consensus Algorithm, it resolves conflicts
+      by replacing our chain with the longest one in the network
+
+      :return: <bool> True if our chain was replaced, False if not
+    """
+    neighbors = self.nodes
+    new_chain = None
+    
+    # We're only looking for chains longer than ours
+    max_length = len(self.chain)
+   
+    # Grab and verify the chains from all the nodes in our network 
+    for node in neighbors: 
+      response = requests.get(f'http://{node}/chain')
+
+      if response.status_code == 200:
+        length = response.json()['length']
+        chain = response.json()['chain']
+        
+        # Check if the length is longer and the chain is valid
+        if length > max_length and self.valid_chain(chain):
+          max_length = length
+          new_chain = chain
+        
+    if new_chain:
+      self.chain = new_chain
+      return True
+   
+    return False
+
   @staticmethod
   def hash(block):
     """
@@ -74,7 +150,8 @@ class Blockchain(object):
   def proof_of_work(self, last_proof):
     """
       Simple Proof of work Algorithm
-        - Find a number p' such that hash(pp') contains leading 4 zeros, where p is the previous p'
+        - Find a number p' such that hash(pp') contains leading 4 zeros, 
+          where p is the previous p'
         - p is the previous proof, and p' is the new proof
 
      :param last_proof: <int>
@@ -88,7 +165,8 @@ class Blockchain(object):
   
   def valid_proof(self, last_proof, proof): 
     """
-      Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+      Validates the Proof: Does hash(last_proof, proof) contain 4 leading 
+                           zeroes?
       
       :param last_proof: <int> Previous Proof
       :param proof: <int> CUrrent Proof
@@ -113,7 +191,7 @@ def mine():
   # We run the proof of work algorithm to get the next proof
   last_block = blockchain.last_block
   last_proof = last_block['proof']
-  proof = block_chain.proof_of_work(last_proof)
+  proof = blockchain.proof_of_work(last_proof)
   
   # We must receive a reward for finding the proof.
   # The sender is "0" to signify that this node has mined a new coin.
@@ -139,14 +217,15 @@ def mine():
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
   values = request.get_json()
-  
+
   # CHeck that the required fields are in the POST'ed data
   required = ['sender', 'recipient', 'amount']
   if not all(k in values for k in required):
     return 'Missing values', 400
 
   # Create a new Transaction
-  index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+  index = blockchain.new_transaction(values['sender'], values['recipient'], 
+                                     values['amount'])
 
   response = {'message': f'Transaction will be added to Block {index}'} 
   return jsonify(response), 201
@@ -159,5 +238,40 @@ def full_chain():
   }
   return jsonify(response), 200
 
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+  values = request.get_json()
+ 
+  nodes = values.get('nodes')
+  
+  if nodes is None:
+    return "Error: Please supply a valid list of nodes", 400
+
+  for node in nodes:
+    blockchain.register_node(node)
+
+  response = {
+    'message': 'New nodes have been added',
+    'total_nodes': list(blockchain.nodes)
+  }
+  return jsonify(response), 201 
+  
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+  replaced = blockchain.resolve_conflicts()
+
+  if replaced:
+    response = {
+      'message': 'Our chain was replaced',
+      'new_chain': blockchain.chain
+    }
+  else:
+    response = {
+      'message': 'Our chain is authoritative',
+      'chain': blockchain.chain
+    }
+
+  return jsonify(response), 200
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5001)
